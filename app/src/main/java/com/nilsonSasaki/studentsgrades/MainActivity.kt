22dc2110d65e0fd755fc08f9d44e9a971483608d
nilsonsasaki <nilsonsasaki@gmail.com>
2021-08-03 16:50:14 -0300

    refactored to remove deprecated plugin

--- app/src/main/java/com/nilsonSasaki/studentsgrades/MainActivity.kt
+++ app/src/main/java/com/nilsonSasaki/studentsgrades/MainActivity.kt
@@ -4,32 +4,39 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btResult:Button = Calculate
-        val tvResult:TextView = DisplayResult
+        val btCalculate: Button = findViewById(R.id.btCalculate)
+        btCalculate.setOnClickListener { calculate() }
 
-        btResult.setOnClickListener{
+    }
 
-            val score1:Int= Integer.parseInt(Exam1.text.toString())
-            val score2:Int= Integer.parseInt(Exam2.text.toString())
-            val average = (score1 + score2)/2.0
-            val abstention = Integer.parseInt(Absences.text.toString())
+    private fun calculate() {
+        val etExam1: EditText = findViewById(R.id.etExam1)
+        val etExam2: EditText = findViewById(R.id.etExam2)
+        val etAbsence: EditText = findViewById(R.id.etAbsences)
+        val tvResults: TextView = findViewById(R.id.tvResults)
 
-            if(average>=6 && abstention<=10){
-                tvResult.setText("Student is approved" + "\n"+ "Final Grade: " + average + "\n" + "Absences: " + abstention)
-                tvResult.setTextColor(Color.GREEN)
-            } else {
-                tvResult.setText("Student is reproved" + "\n"+ "Final Grade: " + average + "\n" + "Absences: " + abstention)
-                tvResult.setTextColor(Color.RED)
-            }
+        val exam1 = etExam1.text.toString().toDouble()
+        val exam2 = etExam2.text.toString().toDouble()
+        val absences = etAbsence.text.toString().toInt()
 
+        val average = (exam1 + exam2) / 2
+
+        if (average >= 6 && absences <= 10) {
+            tvResults.setText("Student is approved\nFinal Grade: $average\nAbsences: $absences")
+            tvResults.setTextColor(Color.GREEN)
+        } else {
+            tvResults.setText("Student is reproved\nFinal Grade: $average\nAbsences: $absences")
+            tvResults.setTextColor(Color.RED)
         }
+
+
     }
 }
