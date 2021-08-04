5522a633cec8d41ad01986210504f53e88686026
nilsonsasaki <nilsonsasaki@gmail.com>
2021-08-04 11:15:20 -0300

    added hide keyboard function, updated result string

--- app/src/main/java/com/nilsonSasaki/studentsgrades/MainActivity.kt
+++ app/src/main/java/com/nilsonSasaki/studentsgrades/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.nilsonSasaki.studentsgrades
 
+import android.app.Activity
+import android.content.Context
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -27,16 +31,27 @@ class MainActivity : AppCompatActivity() {
         val exam2 = etExam2.text.toString().toDouble()
         val absences = etAbsence.text.toString().toInt()
 
-        val average = (exam1 + exam2) / 2
+        val average = String.format("%.2f", (exam1 + exam2) / 2)
 
-        if (average >= 6 && absences <= 10) {
-            tvResults.setText("Student is approved\nFinal Grade: $average\nAbsences: $absences")
+        if (average.toDouble() >= 6 && absences <= 10) {
+            tvResults.text = getString(R.string.result_text, "approved", average, absences)
             tvResults.setTextColor(Color.GREEN)
         } else {
-            tvResults.setText("Student is reproved\nFinal Grade: $average\nAbsences: $absences")
+            tvResults.text = getString(R.string.result_text, "reproved", average, absences)
             tvResults.setTextColor(Color.RED)
         }
+        hideKeyboard()
 
 
+    }
+
+    private fun Activity.hideKeyboard() {
+        hideKeyboard(currentFocus ?: View(this))
+    }
+
+    private fun Context.hideKeyboard(view: View) {
+        val inputMethodManager =
+            getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
